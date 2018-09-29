12e99c19e4ad2467eb4cf9a5a7a9a2a4d2fd0d19
Sergey <workintellect@gmail.com>
2018-09-29 15:20:25 +0300

    advanced-zeros/implemented function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  "use strict";
+  let zeros=0, multipliers={}, currentNumber, arr = [], count, j, currentNumberCount;
+    
+    for(let i=2; i<=base; i++) {
+       if(base%i == 0) {
+          if(i != j) {
+            count = 1;
+            multipliers[i] = count;
+            j=i;
+          }else{
+            count++;
+            multipliers[i] = count;
+          }
+          base = base/i;
+          i--;
+       }
+    }
+    
+    for(let key in multipliers) {
+      currentNumber = number;
+      currentNumberCount = 0;
+      while(currentNumber >= 1) {
+        currentNumber = Math.floor(currentNumber/key);
+        currentNumberCount+=currentNumber;
+       } 
+       arr.push(Math.floor(currentNumberCount/multipliers[key]));
+    } 
+
+    return Math.min.apply(Math, arr);
 }
